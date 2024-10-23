d8c800ec73ac3ccd1cf062af4cdf6e09c1452fc9
matternstsr <matternstsr@gmail.com>
2024-10-23 16:15:04 -0500

    fixed 9 AND 14?

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,4 +1,5 @@
 /* 9-binary_tree_height.c */
+
 #include "binary_trees.h"
 
 /**
@@ -10,10 +11,15 @@
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left_height, right_height;
-	
-	if (!tree)
-		return (0);
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
-	return (left_height > right_height ? left_height : right_height);
+
+    if (!tree)
+        return (0);
+
+    /* Recursively get the height of both subtrees */
+    left_height = binary_tree_height(tree->left);
+    right_height = binary_tree_height(tree->right);
+
+    /* If it's a single node (leaf), return 0; otherwise, return max height + 1 */
+    return (left_height || right_height ? 1 +
+			(left_height > right_height ? left_height : right_height) : 0);
 }
