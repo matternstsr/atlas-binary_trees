9644f7baf000a6e149f378ea7a787299370a6e37
matternstsr <matternstsr@gmail.com>
2024-10-22 22:55:18 -0500

    Working on 5-6

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,1 +1,13 @@
 /* 5-binary_tree_is_root.c */
+#include "binary_trees.h"
+
+/**
+* binary_tree_is_root - Checks if a given node is a root
+* @node: PTR to node to check
+*
+* Return: 1 if node is a root, orwise 0
+*/
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	return (node && !node->parent); /* Check node is not NULL & no parent */
+}
--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,1 +1,17 @@
 /* 6-binary_tree_preorder.c */
+#include "binary_trees.h"
+
+/**
+* binary_tree_preorder - Goes back through a binary tree using pre-order
+* @tree: PTR to root node of tree to traverse
+* @func: PTR to a function to call for each node
+*/
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree && func)  /* Check if tree and func are not NULL */
+	{
+		func(tree->n); /* Call func on current node */
+		binary_tree_preorder(tree->left, func); /* go back on left subtree */
+		binary_tree_preorder(tree->right, func); /* go back on right subtree */
+	}
+}
