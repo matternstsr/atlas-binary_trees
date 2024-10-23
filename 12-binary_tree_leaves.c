c42b711007bf787dfb6d9efcc5fc60b6eab1844c
matternstsr <matternstsr@gmail.com>
2024-10-23 15:19:57 -0500

    Working on 11-12

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,1 +1,17 @@
 /* 12-binary_tree_leaves.c */
+#include "binary_trees.h"
+
+/**
+* binary_tree_leaves - Counts the leaves in a binary tree
+* @tree: PTR to the root node of the tree
+*
+* Return: Number of leaves in the tree
+*/
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	if (!tree->left && !tree->right)
+		return (1);
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+}
