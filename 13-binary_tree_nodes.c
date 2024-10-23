c902134bd36c692e90cb81ff6161c1d22f58bb43
matternstsr <matternstsr@gmail.com>
2024-10-23 15:50:07 -0500

    Working on 13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -9,9 +9,10 @@
 */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
+	size_t count;
 	if (!tree)
 		return (0);
-	size_t count = 0;
+	count = 0;
 	if (tree->left || tree->right)
 		count = 1;
 	return (count + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
