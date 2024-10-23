ffd508faae9bdb3fd71c765e3e0d8f18c56b38a0
matternstsr <matternstsr@gmail.com>
2024-10-23 17:24:39 -0500

    working on 15 -16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -43,7 +43,7 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 /**
 * binary_tree_is_perfect - checks if a binary tree is perfect
 * @tree: PTR to the root node of the tree to check
-* 
+*
 * Return: 1 if perfect, 0 otherwise
 */
 int binary_tree_is_perfect(const binary_tree_t *tree)
