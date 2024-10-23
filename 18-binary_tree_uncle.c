2537731a089e46cfb52bb3def1820393ff113ac4
matternstsr <matternstsr@gmail.com>
2024-10-23 18:16:16 -0500

    working on 17-18

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,1 +1,15 @@
 /* 18-binary_tree_uncle.c */
+#include "binary_trees.h"
+
+/**
+* binary_tree_uncle - finds the uncle of a node
+* @node: PTR to the node to find the uncle
+*
+* Return: PTR to the uncle node, or NULL
+*/
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
+		return (NULL);
+	return (binary_tree_sibling(node->parent));
+}
