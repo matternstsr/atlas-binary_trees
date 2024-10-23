01ebd952c17ef0fd19b1214b8366321e1e1371ba
matternstsr <matternstsr@gmail.com>
2024-10-23 15:48:50 -0500

    Working on 13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,1 +1,18 @@
 /* 13-binary_tree_nodes.c */
+#include "binary_trees.h"
+
+/**
+* binary_tree_nodes - Counts the nodes with at least 1 child in a binary tree
+* @tree: PTR to the root node of the tree
+*
+* Return: Number of nodes with at least 1 child
+*/
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	size_t count = 0;
+	if (tree->left || tree->right)
+		count = 1;
+	return (count + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+}
