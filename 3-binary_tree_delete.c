58ff67081b89c1e84b4d4674be8180c46ba2d124
matternstsr <matternstsr@gmail.com>
2024-10-22 22:41:01 -0500

    Finishing 1-2

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,1 +1,16 @@
 /* 3-binary_tree_delete.c */
+#include "binary_trees.h"
+
+/**
+* binary_tree_delete - Deletes an entire binary tree
+* @tree: Pointer to the root node of the tree to delete
+*/
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree) /* Check if tree is NULL */
+	{
+		binary_tree_delete(tree->left);  /* Delete left subtree */
+		binary_tree_delete(tree->right); /* Delete right subtree */
+		free(tree); /* Free the current node */
+	}
+}
--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,1 +1,16 @@
 /* 4-binary_tree_is_leaf.c */
+#include "binary_trees.h"
+
+/**
+* binary_tree_is_leaf - Checks if a node is a leaf
+* @node: Pointer to the node to check
+*
+* Return: 1 if node is a leaf, otherwise 0
+*/
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	/* Check node is not NULL & no children */
+	if (node && !node->left && !node->right)
+		return (1);
+	return (0);
+}
