9a83cbf25e2783e63177ac08a391c72b2b1810d0
matternstsr <matternstsr@gmail.com>
2024-10-23 16:25:31 -0500

    fixed 9 AND 14?

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,20 +1,20 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_balance - Measures the balance factor of a binary tree
- * @tree: Pointer to the root node of the tree
- *
- * Return: Balance factor of the tree
- */
+* binary_tree_balance - Measures the balance factor of a binary tree
+* @tree: Pointer to the root node of the tree
+*
+* Return: Balance factor of the tree
+*/
 int binary_tree_balance(const binary_tree_t *tree)
 {
 	int left_height, right_height;
 
-    if (!tree)
-        return (0);
-    left_height = binary_tree_height(tree->left);
-    int right_height = binary_tree_height(tree->right);
-    return (left_height - right_height);
+	if (!tree)
+		return (0);
+	left_height = binary_tree_height(tree->left);
+	int right_height = binary_tree_height(tree->right);
+	return (left_height - right_height);
 }
 
 /**
