c53e8fddf3827419bc1da7830e54474ad328e816
matternstsr <matternstsr@gmail.com>
2024-10-23 16:27:13 -0500

    fixed 9 AND 14?

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,28 +8,28 @@
 */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int left_height, right_height;
-
 	if (!tree)
 		return (0);
-	left_height = binary_tree_height(tree->left);
+
+	int left_height = binary_tree_height(tree->left);
 	int right_height = binary_tree_height(tree->right);
+
 	return (left_height - right_height);
 }
 
 /**
-* binary_tree_height - Measures the height of a binary tree
-* @tree: PTR to the root node of the tree to measure the height
+* binary_tree_height - Helper function to measure the height of a binary tree
+* @tree: Pointer to the root node of the tree
 *
 * Return: Height of the tree
 */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height, right_height;
-
 	if (!tree)
 		return (0);
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
+
+	size_t left_height = binary_tree_height(tree->left);
+	size_t right_height = binary_tree_height(tree->right);
+
 	return (left_height > right_height ? left_height : right_height) + 1;
 }
