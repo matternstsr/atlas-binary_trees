e9a9e1546814eb02b1df0de26901d1241d1e5cf9
matternstsr <matternstsr@gmail.com>
2024-10-23 15:51:32 -0500

    Working on 13 missing blank /n

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -10,10 +10,12 @@
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	size_t count;
+	
 	if (!tree)
 		return (0);
 	count = 0;
 	if (tree->left || tree->right)
 		count = 1;
-	return (count + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+	return (count + binary_tree_nodes(tree->left) +
+			binary_tree_nodes(tree->right));
 }
