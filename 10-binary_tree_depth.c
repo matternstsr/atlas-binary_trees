cb5b49d29d3a5d0b3392ed9d0b8d4919fc5dd2df
matternstsr <matternstsr@gmail.com>
2024-10-23 14:49:03 -0500

    Working on 9-10

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,1 +1,20 @@
 /* 10-binary_tree_depth.c */
+#include "binary_trees.h"
+
+/**
+* binary_tree_depth - Measures the depth of a node in a binary tree
+* @tree: PTR to the node to measure the depth
+*
+* Return: Depth of the node
+*/
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t depth = 0;
+
+	while (tree && tree->parent)
+	{
+		depth++;
+		tree = tree->parent;
+	}
+	return (depth);
+}
--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,1 +1,17 @@
 /* 9-binary_tree_height.c */
+#include "binary_trees.h"
+
+/**
+* binary_tree_height - Measures the height of a binary tree
+* @tree: PTR to the root node of the tree to measure the height
+*
+* Return: Height of the tree
+*/
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	size_t left_height = binary_tree_height(tree->left);
+	size_t right_height = binary_tree_height(tree->right);
+	return (left_height > right_height ? left_height : right_height) + 1;
+}
